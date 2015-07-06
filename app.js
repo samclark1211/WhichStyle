cdd64f1ae5b69f8ce11751683b859091137aa302
yusuffaraji <yusuf.faraji@digital.hmrc.gov.uk>
2015-07-06 14:51:49 +0100

    Add Yaml generator

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 //Set up routes import
 var index = require("./routes/routes")
 
 var app = express();
 var router = express.Router();
+//set body parser for json
+app.use(bodyParser());
 
 app.set('view engine', 'handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'));
 
-
+app.post('/create', function(req, res){
+  res.render('create/createTemplate', {layout: false, data: req.body});
+});
 
 // apply the routes to our application
 app.get('/', index.index);
 app.get('/guide/:guideName', index.guide);
-
+app.get('/create', index.create);
 
 var server = app.listen(3000, function () {
 
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,10 @@ exports.index = function(req,res){
     res.render('home', {"fileList": fileList});
 },
 
+exports.create = function(req, res) {
+  res.render('create/createYaml');
+},
+
 exports.guide = function(req, res){
   var parsedYaml = guideRenderer.transform(req.params.guideName);
   res.render('guides/generic', parsedYaml);
