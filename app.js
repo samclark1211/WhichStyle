0fe9f516d02a5683eb16cac0122dd633b815e61f
yusuffaraji <yusuf.faraji@digital.hmrc.gov.uk>
2015-07-06 15:34:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,11 +24,8 @@ app.get('/', index.index);
 app.get('/guide/:guideName', index.guide);
 app.get('/create', index.create);
 
-var server = app.listen(3000, function () {
+app.set('port', (process.env.PORT || 5000));
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('WhichStyle listening at http://%s:%s', host, port);
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
